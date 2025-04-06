3bee06276f13de8df095d8b66ee7fb8c959621f6
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2025-04-07 02:24:31 +0530

    CrossComponent Communication

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,11 +1,16 @@
 import React, { useRef } from "react";
 
-const NewTodo = () => {
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo = (props: NewTodoProps) => {
   const textInputRef = useRef<HTMLInputElement>(null);
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const enteredText = textInputRef.current!.value;
     console.log(enteredText);
+    props.onAddTodo(enteredText);
   };
   return (
     <form onSubmit={todoSubmitHandler}>
